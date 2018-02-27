eebbfc0cb58c1836da35dac0b448ff9a19288294
rickle-msft <frley@microsoft.com>
2018-02-27 15:24:53 -0800

    Cleaned up containerAPI tests by moving a bunch of stuff to the TestUtility

--- azure-storage/src/test/java/com/microsoft/azure/storage/ContainerAPI.groovy
+++ azure-storage/src/test/java/com/microsoft/azure/storage/ContainerAPI.groovy
@@ -1,13 +1,5 @@
 package com.microsoft.azure.storage
 
-import com.linkedin.flashback.SceneAccessLayer
-import com.linkedin.flashback.factory.SceneFactory
-import com.linkedin.flashback.matchrules.CompositeMatchRule
-import com.linkedin.flashback.matchrules.MatchBody
-import com.linkedin.flashback.matchrules.MatchMethod
-import com.linkedin.flashback.matchrules.MatchRuleUtils
-import com.linkedin.flashback.scene.Scene
-import com.linkedin.flashback.scene.SceneConfiguration
 import com.linkedin.flashback.scene.SceneMode
 import com.linkedin.flashback.smartproxy.FlashbackRunner
 import com.microsoft.azure.storage.blob.ContainerAccessConditions
@@ -24,243 +16,75 @@ import com.microsoft.azure.storage.models.Container
 import com.microsoft.azure.storage.models.ContainerCreateHeaders
 import com.microsoft.azure.storage.models.PublicAccessType
 import com.microsoft.rest.v2.RestException
-import com.microsoft.rest.v2.http.HttpClient
 import com.microsoft.rest.v2.http.HttpPipeline
-import org.joda.time.DateTime
-import org.testng.annotations.Test
 import spock.lang.*
 
 class ContainerAPI extends Specification {
 
-    static ServiceURL su
-
     @Shared
     int testNo = 0 // Used to generate stable container names for recordings
 
-    String containerName = TestUtility.enableRecordings ? TestUtility.containerPrefix + testNo :
-            TestUtility.containerPrefix + System.currentTimeMillis()
-
     ContainerURL cu
 
-    String sceneName = "WillNeverBeWrittenTo" // Because the the setup for the first test will change the file
-
-    @Shared
-    FlashbackRunner flashbackRunner
-
-    @Shared
-    CompositeMatchRule rule
-
-    /*
-    The values below are used to create data-driven tests for access conditions.
-     */
-    @Shared
-    Date oldDate = new DateTime().minusDays(1).toDate()
-
-    @Shared
-    Date newDate = new DateTime().plusDays(1).toDate()
-
-    /*
-    Note that this value is only used to check if we are depending on the received etag. This value will not actually
-    be used.
-     */
-    @Shared
-    ETag receivedEtag = new ETag("received")
-
-    @Shared
-    ETag garbageEtag = new ETag("garbage")
-
-    static SharedKeyCredentials creds
-
-    def setupSpec() {
-        creds = new SharedKeyCredentials(System.getenv().get("ACCOUNT_NAME"),
-                System.getenv().get("ACCOUNT_KEY"))
-
-        PipelineOptions po = new PipelineOptions()
-        if (TestUtility.enableDebugging) {
-            HttpClient.Configuration configuration = new HttpClient.Configuration(
-                    new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 8888)))
-            po.client = HttpClient.createDefault(configuration)
-        }
-        else if (TestUtility.enableRecordings) {
-            HttpClient.Configuration configuration = new HttpClient.Configuration(
-                    new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 1234)))
-            po.client = HttpClient.createDefault(configuration)
-            configureMatchRule()
-        }
-
-        HttpPipeline pipeline = StorageURL.createPipeline(creds, po)
-
-        su = new ServiceURL(
-                new URL("http://" + System.getenv().get("ACCOUNT_NAME") + ".blob.core.windows.net"), pipeline)
-    }
-
-    def configureMatchRule() {
-        rule = new CompositeMatchRule()
-        rule.addRule(new MatchBody())
-        rule.addRule(new MatchMethod())
-
-        /*
-         We can ignore the access condition headers because they will be
-         distinguished by the container name when the test unrolls.
-         */
-        HashSet<String> blacklistHeaders = new HashSet<>()
-        blacklistHeaders.add("Authorization")
-        blacklistHeaders.add("x-ms-date")
-        blacklistHeaders.add("x-ms-client-request-id")
-        blacklistHeaders.add("If-Modified-Since")
-        blacklistHeaders.add("If-Unmodified-Since")
-        blacklistHeaders.add("If-Match")
-        blacklistHeaders.add("If-None-Match")
-        rule.addRule(MatchRuleUtils.matchHeadersWithBlacklist(blacklistHeaders))
-
-        HashSet<String> blacklistQuery = new HashSet<>()
-        blacklistQuery.add("sig")
-
-        rule.addRule(MatchRuleUtils.matchUriWithQueryBlacklist(blacklistQuery))
+    def generateContainerName() {
+        String suffix = TestUtility.enableRecordings ? testNo.toString() : System.currentTimeMillis().toString()
+        TestUtility.generateContainerName("",
+                specificationContext.currentFeature.name.replace(' ', '').toLowerCase(),
+                suffix.toString())
     }
 
     def cleanupSpec() {
-        if (TestUtility.enableRecordings) {
-            flashbackRunner.close()
-        }
-
-        // We don't need to clean up containers if we are playing back
-        if (!(TestUtility.enableRecordings && TestUtility.sceneMode.equals(SceneMode.PLAYBACK))) {
-            // Create a new pipeline without any proxies
-            HttpPipeline pipeline = StorageURL.createPipeline(creds, new PipelineOptions())
-
-            ServiceURL serviceURL = new ServiceURL(
-                    new URL("http://" + System.getenv().get("ACCOUNT_NAME") + ".blob.core.windows.net"), pipeline)
-            // There should not be more than 50000 containers from these tests
-            for (Container c : serviceURL.listContainers(null,
-                    new ListContainersOptions(null, TestUtility.containerPrefix, null)).blockingGet().body()
-                    .containers()) {
-                ContainerURL containerURL = serviceURL.createContainerURL(c.name())
-                containerURL.delete(null).blockingGet()
-            }
-        }
+        TestUtility.cleanupContainers()
     }
 
     def setup() {
-        cu = su.createContainerURL(containerName)
-
-        if (TestUtility.enableRecordings) {
-            sceneName = specificationContext.getCurrentIteration().name
-            SceneConfiguration sceneConfig =
-                    new SceneConfiguration(TestUtility.sceneDir, TestUtility.sceneMode, sceneName)
-            flashbackRunner = new FlashbackRunner.Builder().host("localhost").port(1234)
-                    .mode(TestUtility.sceneMode)
-                    .sceneAccessLayer(new SceneAccessLayer(SceneFactory.create(sceneConfig), rule))
-                    .build()
-            flashbackRunner.start()
-        }
+        cu = TestUtility.getPrimaryServiceURL().createContainerURL(generateContainerName())
+        TestUtility.setupFeatureRecording(specificationContext.getCurrentIteration().name)
     }
 
     def cleanup() {
         testNo++
-        // TODO: If this there is a new test, this will fail because the file isn't created until
-        // the flashbackRunner closes and writes. Should move this to the cleanupSpec and list out the dir?
-        // In that case I'll need a different dir per spec, which is fine.
-        //TestUtility.scrubAuthHeader(sceneName)
-        if (TestUtility.enableRecordings) {
-            flashbackRunner.close()
-        }
-
+        TestUtility.cleanupFeatureRecording()
     }
 
     @Unroll
     def "Delete container with access conditions"() {
         setup:
         ContainerCreateHeaders headers = cu.create(null, PublicAccessType.BLOB).blockingGet().headers()
-        if (match.equals(receivedEtag)) {
+        if (match.equals(TestUtility.receivedEtag)) {
             match = new ETag(headers.eTag())
         }
         ContainerAccessConditions cac = new ContainerAccessConditions(
                 new HTTPAccessConditions(modified, unmodified, match, noneMatch), LeaseAccessConditions.NONE)
         int code = 0
-        //TODO: Have to use this to actually check the exception message in the expect section
-        String message = ""
+
+        boolean foundErrorMessage = false
         try{
             code = cu.delete(cac).blockingGet().statusCode()
         }
         catch (RestException e) {
             code = e.response().statusCode()
-            //cu.delete(null).blockingGet() // If we were testing a failure case, still clean up the container
+            // We don't need to check the error message here because we check the status code.
         }
         catch (IllegalArgumentException e) {
-            e.message.contains("ETag access conditions are not supported")
-            //cu.delete(null).blockingGet()
+            foundErrorMessage = e.message.contains("ETag access conditions are not supported")
         }
         expect:
         code == statusCode
+        foundErrorMessage == foundMessage
 
         /*
         For some reason, putting the 400 cases first works fine but putting them later in the sequence
         yields some sort of timeout error.
          */
         where:
-        modified           | unmodified      | match          | noneMatch        || statusCode
-        newDate            | null            | null           | null             || 412
-        null               | oldDate         | null           | null             || 412
-        null               | null            | null           | null             || 202
-        oldDate            | null            | null           | null             || 202
-        null               | newDate         | null           | null             || 202
-        null               | null            | receivedEtag   | null             || 0
-        null               | null            | null           | garbageEtag      || 0
-    }
-
-    def "Test the failed data driven case"() {
-        setup:
-        Date modified = newDate
-        Date unmodified = null
-        ETag match = null
-        ETag noneMatch = null
-        ContainerCreateHeaders headers = cu.create(null, PublicAccessType.BLOB).blockingGet().headers()
-        if (match.equals(receivedEtag)) {
-            match = new ETag(headers.eTag())
-        }
-        ContainerAccessConditions cac = new ContainerAccessConditions(
-                new HTTPAccessConditions(modified, unmodified, match, noneMatch), LeaseAccessConditions.NONE)
-        int code = 0
-        try{
-            code = cu.delete(cac).blockingGet().statusCode()
-        }
-        catch (RestException e) {
-            code = e.response().statusCode()
-            //cu.delete(null).blockingGet() // If we were testing a failure case, still clean up the container
-        }
-        catch (IllegalArgumentException e) {
-            e.message.contains("ETag access conditions are not supported")
-            //cu.delete(null).blockingGet()
-        }
-        expect:
-        code == 412
-    }
-
-    @Unroll
-    def "Test recorded failed response"() {
-        ContainerCreateHeaders headers = cu.create(null, PublicAccessType.BLOB).blockingGet().headers()
-
-        ContainerAccessConditions cac
-        if (succeed) {
-            cac = ContainerAccessConditions.NONE
-        }
-        else {
-            cac = new ContainerAccessConditions(HTTPAccessConditions.NONE,
-                    new LeaseAccessConditions("lease"))
-        }
-
-        expect:
-        try {
-            cu.delete(cac).blockingGet()
-        }
-        catch (RestException e) {
-            e.response().statusCode() == result
-        }
-
-        where:
-        succeed || result
-        true    || 202
-        false   || 400
+        modified            | unmodified          | match                    | noneMatch               || statusCode | foundMessage
+        TestUtility.newDate | null                | null                     | null                    || 412        | false
+        null                | TestUtility.oldDate | null                     | null                    || 412        | false
+        null                | null                | null                     | null                    || 202        | false
+        TestUtility.oldDate | null                | null                     | null                    || 202        | false
+        null                | TestUtility.newDate | null                     | null                    || 202        | false
+        null                | null                | TestUtility.receivedEtag | null                    || 0          | true
+        null                | null                | null                     | TestUtility.garbageEtag || 0          | true
     }
 }
--- azure-storage/src/test/java/com/microsoft/azure/storage/TestUtility.java
+++ azure-storage/src/test/java/com/microsoft/azure/storage/TestUtility.java
@@ -1,33 +1,179 @@
 package com.microsoft.azure.storage;
 
+import com.linkedin.flashback.SceneAccessLayer;
+import com.linkedin.flashback.factory.SceneFactory;
+import com.linkedin.flashback.matchrules.*;
+import com.linkedin.flashback.scene.SceneConfiguration;
 import com.linkedin.flashback.scene.SceneMode;
-import jdk.jfr.events.ExceptionThrownEvent;
+import com.linkedin.flashback.smartproxy.FlashbackRunner;
+import com.microsoft.azure.storage.blob.*;
+import com.microsoft.azure.storage.models.Container;
+import com.microsoft.rest.v2.http.HttpClient;
+import com.microsoft.rest.v2.http.HttpPipeline;
+import org.joda.time.DateTime;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.URL;
+import java.security.InvalidKeyException;
+import java.util.Date;
+import java.util.HashSet;
 
 public class TestUtility {
 
-    public static boolean enableDebugging = false;
+    public static final boolean enableDebugging = false;
 
-    public static boolean enableRecordings = true;
+    public static final boolean enableRecordings = true;
 
-    public static String containerPrefix = "javatestcontainer";
+    public static final String containerPrefix = "javatestcontainer";
 
-    public static SceneMode sceneMode = SceneMode.PLAYBACK;
+    public static final SceneMode sceneMode = SceneMode.PLAYBACK;
 
     public static final String sceneDir =
             "C:\\Users\\frley\\Documents\\azure-storage-java-async\\azure-storage\\src\\test\\resources\\recordings\\";
 
-    public static void scrubAuthHeader(String sceneName) {
-        try {
-            FileReader fr = new FileReader(sceneDir + sceneName);
+    private static FlashbackRunner flashbackRunner;
 
+    private static CompositeMatchRule matchRule = null;
+
+    /*
+    The values below are used to create data-driven tests for access conditions.
+     */
+    public static final Date oldDate = new DateTime().minusDays(1).toDate();
+
+    public static final Date newDate = new DateTime().plusDays(1).toDate();
+
+    /*
+    Note that this value is only used to check if we are depending on the received etag. This value will not actually
+    be used.
+     */
+    public static final ETag receivedEtag = new ETag("received");
+
+    public static final ETag garbageEtag = new ETag("garbage");
+
+    private static ServiceURL primaryServiceURL = null;
+
+    private static SharedKeyCredentials primaryCreds = null;
+
+    public static MatchRule getMatchRule() {
+        if (matchRule == null) {
+            matchRule = new CompositeMatchRule();
+            matchRule.addRule(new MatchBody());
+            matchRule.addRule(new MatchMethod());
+
+        /*
+         We can ignore the access condition headers because they will be
+         distinguished by the container name when the test unrolls.
+         */
+            HashSet<String> blacklistHeaders = new HashSet<>();
+            blacklistHeaders.add("Authorization");
+            blacklistHeaders.add("x-ms-date");
+            blacklistHeaders.add("x-ms-client-request-id");
+            blacklistHeaders.add("If-Modified-Since");
+            blacklistHeaders.add("If-Unmodified-Since");
+            blacklistHeaders.add("If-Match");
+            blacklistHeaders.add("If-None-Match");
+            matchRule.addRule(MatchRuleUtils.matchHeadersWithBlacklist(blacklistHeaders));
+
+            HashSet<String> blacklistQuery = new HashSet<>();
+            blacklistQuery.add("sig");
+
+            matchRule.addRule(MatchRuleUtils.matchUriWithQueryBlacklist(blacklistQuery));
         }
-        catch (Exception e) {
-            throw new Error(e);
+        return matchRule;
+    }
+
+    public static String generateContainerName(String prefix, String core, String suffix) {
+        return containerPrefix + prefix + core + suffix;
+    }
+
+    public static void setupFeatureRecording(String sceneName) {
+        if (enableRecordings) {
+            try {
+                SceneConfiguration sceneConfig =
+                        new SceneConfiguration(TestUtility.sceneDir, TestUtility.sceneMode, sceneName);
+                FlashbackRunner runner = new FlashbackRunner.Builder().host("localhost").port(1234)
+                        .mode(TestUtility.sceneMode)
+                        .sceneAccessLayer(new SceneAccessLayer(SceneFactory.create(sceneConfig),
+                                TestUtility.getMatchRule()))
+                        .build();
+                runner.start();
+                flashbackRunner = runner;
+            }
+            catch (IOException | InterruptedException e) {
+                throw new Error(e);
+            }
+        }
+    }
+
+    public static void cleanupFeatureRecording() {
+        if (enableRecordings) {
+            flashbackRunner.close();
+        }
+        if (enableRecordings) {
+            if (sceneMode.equals(SceneMode.RECORD)) {
+                // TODO: Needed?
+                //TestUtility.scrubAuthHeader(sceneName)
+            }
+        }
+    }
+
+    public static void scrubAuthHeader(String sceneName) {
+        /*try (Scanner scanner = new Scanner(new FileInputStream(sceneDir + sceneName))) {
+            while(scanner.hasNext("\"Authorization\" :")) {
+
+            }
+        } catch (IOException e) {
+
+        }*/
+    }
+
+    public static SharedKeyCredentials getPrimaryCreds() throws InvalidKeyException {
+        if (primaryCreds == null) {
+            primaryCreds = new SharedKeyCredentials(System.getenv().get("ACCOUNT_NAME"),
+                    System.getenv().get("ACCOUNT_KEY"));
+        }
+        return primaryCreds;
+    }
+
+    public static ServiceURL getPrimaryServiceURL() throws InvalidKeyException, MalformedURLException {
+        if (primaryServiceURL == null) {
+            PipelineOptions po = new PipelineOptions();
+            if (TestUtility.enableDebugging) {
+                HttpClient.Configuration configuration = new HttpClient.Configuration(
+                        new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 8888)));
+                po.client = HttpClient.createDefault(configuration);
+            } else if (TestUtility.enableRecordings) {
+                HttpClient.Configuration configuration = new HttpClient.Configuration(
+                        new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 1234)));
+                po.client = HttpClient.createDefault(configuration);
+            }
+
+            HttpPipeline pipeline = StorageURL.createPipeline(getPrimaryCreds(), po);
+
+            primaryServiceURL = new ServiceURL(
+                    new URL("http://" + getPrimaryCreds().getAccountName() + ".blob.core.windows.net"), pipeline);
+        }
+        return primaryServiceURL;
+    }
+
+    public static void cleanupContainers() throws MalformedURLException {
+        // We don't need to clean up containers if we are playing back
+        if (!(TestUtility.enableRecordings && TestUtility.sceneMode.equals(SceneMode.PLAYBACK))) {
+            // Create a new pipeline without any proxies
+            HttpPipeline pipeline = StorageURL.createPipeline(primaryCreds, new PipelineOptions());
+
+            ServiceURL serviceURL = new ServiceURL(
+                    new URL("http://" + System.getenv().get("ACCOUNT_NAME") + ".blob.core.windows.net"), pipeline);
+            // There should not be more than 50000 containers from these tests
+            for (Container c : serviceURL.listContainers(null,
+                    new ListContainersOptions(null, TestUtility.containerPrefix, null)).blockingGet()
+                    .body().containers()) {
+                ContainerURL containerURL = serviceURL.createContainerURL(c.name());
+                containerURL.delete(null).blockingGet();
+            }
         }
     }
 }
